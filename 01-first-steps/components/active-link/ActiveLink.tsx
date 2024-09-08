fb1fa3d00810d771aa197632515f003204ff75c4
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2024-09-08 16:35:46 -0600

    03 - Despliegues a Vercel y Docker Images

--- 01-first-steps/components/active-link/ActiveLink.tsx
+++ 01-first-steps/components/active-link/ActiveLink.tsx
@@ -14,9 +14,10 @@ export const ActiveLink = ({path, text}:Props) => {
 
   return (
     <Link 
-        className={ `${style.link} ${ (pathName == path) && style['active-link']}` }
-        href={path}>
-            {text}
+      // prefetch={true}
+      className={ `${style.link} ${ (pathName == path) && style['active-link']}` }
+      href={path}>
+          {text}
     </Link>
   )
 }
